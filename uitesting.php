80c58881e5f3b3f8126485377e10fff45ef0b393
JEAN Witch Leyder <jeanwitchleyder@gmail.com>
2025-12-16 17:33:02 +0800

    CSS cleaning/Adding new section

--- uitesting.php
+++ uitesting.php
@@ -36,14 +36,85 @@ $error = '';
     </section>
    ---->
 
-     <div class="home-mega-section">
+<section class="home-mega-section">
        <div class="title">
-         <h1>Education,technologie et Solution réelles</h1> 
+       <h1>Education,technologie et Solution réelles</h1> 
        </div>
-       <div class="technology-section">
+
+
+
+       <section class="technology-section">
+         <div class="tech-grid-element">
+         <h2 class="tech-grid-title">Une école pensée pour l'ère numérique</h2>
+         </div>
+         <div class="title tech-grid-element">
+         <h3 class="tech-grid-title">Site web officiel</h3>
+         </div>
+         <div class="tech-grid-element">
+         <h3 class="tech-grid-title">Système de gestion scolaire intégré</h3>
+         </div>
+         <div class="tech-grid-element">
+          <h3 class="tech-grid-title">Portail élèves/parents/enseignants</h3>
+         </div>
+         <div class="tech-grid-element">
+            
+         </div>
          
        </div>
-     </div>
+        </section>
+ 
+        <div class="title">
+       <h1>Payez la scolarité via Moncash</h1> 
+       </div>
+
+        <section class="moncash-section">
+        <div class="moncash-section-element">
+         <div class="title">
+            <h3 class="moncash-section-text">Paiment sécurisé via Moncash</h3>
+         </div>
+         <p class="moncash-section-text">Pas besoin de quitter le travail simplement 
+            pour verser la scolarité. </p>
+            <p class="moncash-section-text">Paie la scolarité en deux clics. </p>
+        </div>
+        <div class="moncash-section-element">
+        
+        </div>
+      
+        </section>
+
+        <div class="title">
+       <h1>Nos Développeurs</h1> 
+       </div>
+
+
+        <section class="ourdevelopers-section">
+         <div class="first-developer">
+         <div class="first-developer-part-1">
+         <img src="images/0016_3.JPG" alt="">
+         </div>
+         <div class="first-developer-part-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam amet vitae mollitia animi sed illo. Ea earum quaerat itaque aliquam dolorum saepe placeat, et facere deleniti omnis neque tempora aperiam.
+            Corporis impedit quisquam dignissimos? Aspernatur unde doloremque nobis harum sapiente in accusantium qui eveniet. Itaque numquam esse dolore in porro harum. Culpa laboriosam explicabo doloremque laborum dolorem, cum doloribus accusamus?
+            Corporis tempora voluptas, rem error ipsa recusandae temporibus ullam, aut, iusto ipsam minima expedita nesciunt impedit accusamus autem porro? Eos cupiditate, illo aspernatur voluptate nesciunt ducimus possimus modi veritatis quam!
+         </div> 
+         </div>
+         <div class="second-developer">
+         <div class="second-developer-part-1">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam amet vitae mollitia animi sed illo. Ea earum quaerat itaque aliquam dolorum saepe placeat, et facere deleniti omnis neque tempora aperiam.
+            Corporis impedit quisquam dignissimos? Aspernatur unde doloremque nobis harum sapiente in accusantium qui eveniet. Itaque numquam esse dolore in porro harum. Culpa laboriosam explicabo doloremque laborum dolorem, cum doloribus accusamus?
+            Corporis tempora voluptas, rem error ipsa recusandae temporibus ullam, aut, iusto ipsam minima expedita nesciunt impedit accusamus autem porro? Eos cupiditate, illo aspernatur voluptate nesciunt ducimus possimus modi veritatis quam!
+         
+         </div> 
+         <div class="second-developer-part-2">
+         <img src="images/0016_3.JPG" alt="">
+         </div>
+         
+         </div>
+        </section>
+        
+    
+
+</section>
     
     
 
